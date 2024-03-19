cd46ea324ab23cb2afdd3de8816c2d8f2e7722c1
Murali Krishna <muralikrishnat.javadev@gmail.com>
2024-03-19 12:03:59 +0530

    First1

---
+++ D.java
@@ -0,0 +1,9 @@
+public class D
+{
+   public static void main(String[] args)
+     {
+         System.out.println("hello Murali Krishna ");
+         System.out.println("hello Munna Krishna ");
+
+      }
+  }
