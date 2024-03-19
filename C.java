0e037e2f4161f51a5a09edfe062576ab2ac1bd60
Murali Krishna <muralikrishnat.javadev@gmail.com>
2024-03-19 12:13:26 +0530

    first

--- C.java
+++ C.java
@@ -3,5 +3,7 @@ public class B
    public static void main(String[] args)
      {
          System.out.println("hello Murali Krishna ");
+         System.out.println("hello Murali Krishna2");
+
       }
   }
